0ab8e1abf39b421b4feb296e56f92180c22b2e41
CODEFORBLIND <chelsimanishjain@gmail.com>
2026-03-20 15:29:35 +0530

    intial commit

--- Array/Easy/5.cpp
+++ Array/Easy/5.cpp
@@ -1,6 +1,48 @@
+// Left Rotate the Array by One
+
+// Problem Statement: Given an integer array nums, rotate the array to the left by one.
+
+// Note: There is no need to return anything, just modify the given array.
+
+// Example 1:
+// Input:
+//  nums = [1, 2, 3, 4, 5]  
+// Output:
+//  [2, 3, 4, 5, 1]  
+// Explanation:
+//  Initially, nums = [1, 2, 3, 4, 5]  
+// Rotating once to the left results in nums = [2, 3, 4, 5, 1].
+
+
+
 #include<bits/stdc++.h>
 using namespace std;
+
+
+class arrayrl{
+    public:
+    void rotateleft(int *arr, int n){
+        // Shift the first element to last
+        int sf = arr[0];
+        int dummyarr[n];
+        int j = 0;
+        for (int i = 1; i < n; i++)
+        {
+            dummyarr[j] = arr[i];
+            j++;
+        }
+        dummyarr[j] = arr[0];
+        for (int i = 0; i < n; i++)
+        {
+            cout << dummyarr[i];
+        }
+    }
+};
+
 int main(){
-    
+    arrayrl rl;
+    int arr[] = {1,2,3,4,5};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    rl.rotateleft(arr,n);
 return 0;
 }
