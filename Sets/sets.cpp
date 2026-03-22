bad7164558000697ab29e2e4b1cad23b0382635d
CODEFORBLIND <chelsimanishjain@gmail.com>
2026-03-22 17:02:49 +0530

    intial commit

---
+++ Sets/sets.cpp
@@ -0,0 +1,16 @@
+// Help : https://www.w3schools.com/cpp/cpp_sets.asp
+// A set stores unique elements that are sorted automatically in ascending order.
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    set<int> s1;
+    s1.insert(1);
+    s1.insert(9);
+    s1.insert(3);
+    s1.insert(5);
+
+    // Print the elements in the set
+    
+return 0;
+}
