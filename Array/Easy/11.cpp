f8e4f4fcca67d77764e9eefcafc1fdc17844d92a
CODEFORBLIND <chelsimanishjain@gmail.com>
2026-03-23 23:52:50 +0530

    initial commit

--- Array/Easy/11.cpp
+++ Array/Easy/11.cpp
@@ -14,7 +14,30 @@
 
 #include<bits/stdc++.h>
 using namespace std;
+class ConsecOne{
+    public:
+    void maxConOne(int *arr, int n){
+        int counter = 0;
+        int maxCount = 0;
+        for (int i = 0; i < n; i++)
+        {
+            while (arr[i] != 0)
+            {
+                counter++;
+                i++;
+                cout << counter << " " << endl;
+            }
+            maxCount = counter;
+            cout << "Max Count is : " << maxCount << " " << endl;
+            counter = 0;
+        }
+        cout << "Max Count is : " << maxCount << endl;
+    }
+};
 int main(){
-    
+    ConsecOne co;
+    int arr[] = {1, 1, 0, 1, 1, 1};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    co.maxConOne(arr, n);
 return 0;
 }
