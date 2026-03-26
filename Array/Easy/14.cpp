0e48bcfdfcd51542cb301c706da5f9f5b7a30c5b
CODEFORBLIND <chelsimanishjain@gmail.com>
2026-03-26 20:36:26 +0530

    initial commit

---
+++ Array/Easy/14.cpp
@@ -0,0 +1,15 @@
+// Length of the longest subarray with zero Sum
+
+// Problem Statement: Given an array containing both positive and negative integers, we have to find the length of the longest subarray with the sum of all elements equal to zero.
+
+// Example 1:
+// Input:
+//  N = 6, array[] = {9, -3, 3, -1, 6, -5}  
+// Result:
+//  5  
+// Explanation:
+//  The following subarrays sum to zero:
+// - {-3, 3}
+// - {-1, 6, -5}
+// - {-3, 3, -1, 6, -5}
+// The length of the longest subarray with sum zero is 5.
