fb67a5b128ebfecc094165ff052e51a2386d78ad
CODEFORBLIND <chelsimanishjain@gmail.com>
2026-05-15 22:52:45 +0530

    initial commit

--- Array/Medium/4.cpp
+++ Array/Medium/4.cpp
@@ -22,17 +22,41 @@ using namespace std;
 void largestSum(int arr[], int n){
     int currsum = 0;
     int res = 0;
+
     for (int i = 0; i < n; i++)
     {
         for (int j = i; j < n; j++)
         {
             currsum += arr[j];
             res = max(res, currsum);
+
         }
         currsum = 0;
     }
-    
+
     cout << "Maximum Sum is: " << res << endl;
+
+    // Printing the subarray
+    int sum = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i; j < n; j++)
+        {
+            sum += arr[j];
+            if(sum == res){
+                cout << i << "," << j << endl;
+                for (int k = i; k <= j; k++)
+                {
+                    cout << arr[k] << " ";
+                }
+                cout << endl;
+                
+                return;
+            }
+        }
+        sum = 0;
+    }
 }
 
 // Kadane's Algorithm   
@@ -53,7 +77,7 @@ void kadaneAlgo(int arr[], int n){
 int main(){
     int arr[] = {2, 3, 5, -2, 7, -4};
     int n = sizeof(arr)/sizeof(int);
-    // largestSum(arr, n);
-    kadaneAlgo(arr, n);
+    largestSum(arr, n);
+    // kadaneAlgo(arr, n);
 return 0;
 }
