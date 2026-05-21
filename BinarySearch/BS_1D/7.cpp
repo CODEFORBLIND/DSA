6d51db4b0c8ab997f980445c4b001ca6f8023fb0
CODEFORBLIND <chelsimanishjain@gmail.com>
2026-05-21 14:35:23 +0530

    initial commit

---
+++ BinarySearch/BS_1D/7.cpp
@@ -0,0 +1,20 @@
+// Count Occurrences in Sorted Array
+
+// Problem Statement: You are given a sorted array containing N integers and a number X, you have to find the occurrences of X in the given array.
+
+// Examples
+// Example 1:
+// Input:
+//  N = 7,  X = 3 , array[] = {2, 2 , 3 , 3 , 3 , 3 , 4}
+// Output
+// : 4
+// Explanation:
+//  3 is occurring 4 times in 
+// the given array so it is our answer.
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    
+return 0;
+}
