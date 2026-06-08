40db352830d9c0d8729137293d462f0c0bb9cd6d
CODEFORBLIND <chelsimanishjain@gmail.com>
2026-06-08 22:49:39 +0530

    initial commit

---
+++ Strings/Basics/3.cpp
@@ -0,0 +1,13 @@
+// Largest Odd Number in a String.
+
+// Problem Statement: Given a string s, representing a large integer, the task is to return the largest-valued odd integer (as a string) that is a substring of the given string s.
+// The number returned should not have leading zero's. But the given input string may have leading zero.
+
+// Examples
+// Example 1
+// Input:
+//  s = "5347"
+// Output:
+//  "5347"
+// Explanation:
+//  The odd numbers formed by the given string are → 5, 3, 53, 347, 5347. The largest odd number without leading zeroes is 5347.
