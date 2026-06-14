4848af6b3d5934a1c02b16fde48e57a6e327fc7f
CODEFORBLIND <chelsimanishjain@gmail.com>
2026-06-14 22:16:48 +0530

    initial commit

--- Strings/Basics/3.cpp
+++ Strings/Basics/3.cpp
@@ -15,7 +15,26 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main(){
+string largestOdd(string s){
+    int index = -1;
+    for (int i = s.length() - 1; i >= 0; i++) {
+        if((s[i] - '0') % 2 == 1){
+            index = i;
+            break;
+        }
+    }
+    int i = 0;
+    while (i <= index && s[i] == '0')
+    {
+        i++;
+    }
     
+    return s.substr(i, index - i + 1);
+}
+
+int main(){
+    string s = "5347";
+    string result = largestOdd(s);
+    cout << "Largest Odd Number: " << result << endl;
 return 0;
 }
