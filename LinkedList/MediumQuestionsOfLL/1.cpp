d74e70485249375312de45f83ddff32e233756d4
CODEFORBLIND <chelsimanishjain@gmail.com>
2026-06-27 10:00:53 +0530

    initial commit

---
+++ LinkedList/MediumQuestionsOfLL/1.cpp
@@ -0,0 +1,107 @@
+// Find middle element in a Linked List
+
+// Problem Statement: Given the head of a linked list of integers, determine the middle node of the linked list. However, if the linked list has an even number of nodes, return the second middle node.
+
+// Example 1:
+// Input:
+//  LL: 1  2  3  4  5 
+
+// Output:
+//     3
+// Explanation: 
+// Node with value 3 is the middle node of this linked list.
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class linkedList{
+    private:
+        class Node{
+            public:
+                int data;
+                Node *next;
+
+            Node(int value){
+                data = value;
+            }
+
+            Node(int value, Node* next1){
+                data = value;
+                next = next1;
+            }
+        };
+    public:
+        Node *head;
+        Node *tail;
+
+        linkedList(){
+            head = tail = nullptr;
+        }
+
+        ~linkedList(){
+            Node *temp;
+            while(head){
+                temp = head;
+                head = head -> next;
+                delete head;
+            }
+        }
+
+        void append(int val){
+            Node *temp = new Node(val);
+            if(!head){
+                head = tail = temp;
+            } else {
+                tail ->next = temp;
+                tail = temp;
+            }
+        }
+
+        void midElem(){
+            Node *temp = head;
+            int count = 0;
+            while(temp != nullptr){
+                count++;
+                temp = temp->next;
+            }
+            temp = head;
+            int mid = (count/2) + 1;
+            cout << mid << endl;
+            int counter = 1;
+            while(counter < mid){
+                temp = temp->next;
+                counter++;
+            }
+            cout << temp->data << endl;
+        }
+
+        void printLinkedList(){
+            Node *temp = head;
+            while(temp != nullptr){
+                cout << temp->data << " ";
+                temp = temp->next;
+            }
+            cout << endl;
+        }
+};
+
+int main(){
+    linkedList list;
+    cout << "Original List " << endl;
+    list.append(10);
+    list.append(20);
+    list.append(30);
+    list.append(40);
+    list.append(50);
+    list.append(60);
+    list.append(70);
+    list.printLinkedList();
+    list.midElem();
+    // list.deleteLast();
+    // cout << "List after deletion of last element: " << endl;
+    // list.printLinkedList();
+    // // list.deleteFirst();
+    // // cout << "List after deletion of first element: " << endl;
+    // // list.printLinkedList();
+return 0;
+}
