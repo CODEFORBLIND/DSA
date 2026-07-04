a9934651f836a45e40b1e81af2eab8b3eaf7327c
CODEFORBLIND <chelsimanishjain@gmail.com>
2026-07-04 22:39:21 +0530

    initial commit

---
+++ LinkedList/MediumQuestionsOfLL/9.cpp
@@ -0,0 +1,8 @@
+Remove N-th node from the end of a Linked List
+
+Problem Statement: Given a linked list and an integer N, the task is to delete the Nth node from the end of the linked list and print the updated linked list.
+
+Examples
+Input:  5->1->2, N=2
+Output: 5->2
+Explanation: The 2nd node from the end of the linked list is 1. Therefore, we get this result after removing 1 from the linked list.
