0b0289c714bc101e1cd5fdfdb99a4651b5c3f54a
CODEFORBLIND <chelsimanishjain@gmail.com>
2026-07-09 23:39:50 +0530

    initial commit

--- LinkedList/MediumQuestionsOfLL/9.cpp
+++ LinkedList/MediumQuestionsOfLL/9.cpp
@@ -9,7 +9,79 @@
 
 #include<bits/stdc++.h>
 using namespace std;
+
+class linkedList{
+    public:
+        class Node{
+            public:
+                int data;
+                Node *next;
+                bool visited;
+
+            Node(int value){
+                data = value;
+                next = nullptr;
+                visited = false;
+            }
+
+            Node(int value, Node* next1){
+                data = value;
+                next = next1;
+            }
+        };
+        Node *head;
+        Node *tail;
+
+        linkedList(){
+            head = tail = nullptr;
+        }
+
+        ~linkedList(){
+            Node *temp;
+            while(head){
+                temp = head;
+                head = head -> next;
+                delete temp;
+            }
+        }
+
+        void append(int val){
+            Node *temp = new Node(val);
+            if(!head){
+                head = tail = temp;
+            } else {
+                tail ->next = temp;
+                tail = temp;
+            }
+        }
+
+
+        void printLinkedList(){
+            Node *temp = head;
+            while(temp != nullptr){
+                cout << temp->data << " ";
+                temp = temp->next;
+            }
+            cout << endl;
+        }
+};
+
 int main(){
-    
+    linkedList list;
+    cout << "Original List " << endl;
+    list.append(10);
+    list.append(20);
+    list.append(30);
+    list.append(40);
+    list.append(50);
+    list.append(60);
+    list.append(70);
+    list.printLinkedList();
+    // Making a cycle
+    linkedList::Node* node30 = list.head->next->next;
+    list.tail->next = node30;
+    list.detectCycle();
+    list.tail->next = nullptr;
+
 return 0;
 }
